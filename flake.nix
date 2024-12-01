eebc56c1b219cc3965e0241f6346a0703b35af0c
Zexk <bouraoui@outlook.it>
2024-12-01 01:43:56 +0100

    flake

--- flake.nix
+++ flake.nix
@@ -1,31 +1,45 @@
 {
-  description = "dwm nix flake";
-  
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-  };
+  description = "dwm flake";
 
-  outputs = { self, nixpkgs }:
-      let
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-          overlays = [
-            (final: prev: {
-              dwmPtch = prev.dwm.overrideAttrs (oldAttrs: {
-                version = "master";
-                src = ./.;
-              });
-            })
-          ];
-        };
-      in
-      {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-        packages.x86_64-linux.dwmPtch = pkgs.dwmPtch;
-        packages.x86_64-linux.default = pkgs.dwmPtch;
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forEachSystem = nixpkgs.lib.genAttrs systems;
 
-        devShell.x86_64-linux = pkgs.mkShell {
-          buildInputs = with pkgs; [ xorg.libX11 xorg.libXft xorg.libXinerama gcc bear ];
-        };
-      };
+      overlayList = [ self.overlays.default ];
+
+      pkgsBySystem = forEachSystem (
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = overlayList;
+        }
+      );
+
+    in
+  {
+
+      overlays.default = (final: prev: {
+        dwmPatched = prev.dwm.overrideAttrs (oldAttrs: {
+        version = "master";
+        src = ./.;
+    });
+  });
+      packages = forEachSystem (system: {
+        dwmPatched = pkgsBySystem.${system}.dwmPatched;
+        default = pkgsBySystem.${system}.dwmPatched;
+      });
+
+      nixosModules.overlays = overlayList;
+
+    };
 }
