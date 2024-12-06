c9fa2f9fea664a97a387172ba2783b4b38b90c63
Zexk <bouraoui@outlook.it>
2024-12-06 22:25:44 +0100

    colorscheme

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -15,7 +15,7 @@ static const char col_cyan[]        = "#efeaf9";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan },
+	[SchemeSel]  = { col_cyan, col_gray1,  col_cyan },
 };
 
 /* tagging */
